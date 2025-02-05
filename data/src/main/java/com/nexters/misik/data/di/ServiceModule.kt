88c0d97174eb762e5aea8297046993e7ca3b8504
hyeseon <konkukbhs@gmail.com>
2025-02-05 18:20:00 +0900

    [feat/#14] implement ServiceModule, RepositoryModule

---
+++ data/src/main/java/com/nexters/misik/data/di/ServiceModule.kt
@@ -0,0 +1,21 @@
+package com.nexters.misik.data.di
+
+import com.nexters.misik.data.service.ReviewService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+    @Singleton
+    @Provides
+    internal fun provideReviewService(
+        retrofit: Retrofit,
+    ): ReviewService {
+        return retrofit.create(ReviewService::class.java)
+    }
+}
