47aca97da5e58086c12c25f62d20a52d2d660b82
hyeseon <konkukbhs@gmail.com>
2025-02-10 15:51:33 +0900

    [feat/#17] WebViewState 세분화하여 로직 수정

--- feature/webview/src/main/java/com/nexters/misik/webview/WebViewState.kt
+++ feature/webview/src/main/java/com/nexters/misik/webview/WebViewState.kt
@@ -1,16 +1,10 @@
 package com.nexters.misik.webview
 
-/*data class WebViewState(
-    val isLoading: Boolean = false,
-    val content: String? = null,
-    val error: String? = null,
-    val reviewId: Long = 0,
-    val review: ReviewEntity? = null,
-)*/
-
 sealed class WebViewState {
     data object PageLoading : WebViewState()
     data object PageLoaded : WebViewState()
+    data class GenerateReview(val id: Long) : WebViewState()
+    data class CompleteReview(val review: String) : WebViewState()
     data class ResponseJS(val response: String) : WebViewState()
     data class Error(val message: String) : WebViewState()
 }
