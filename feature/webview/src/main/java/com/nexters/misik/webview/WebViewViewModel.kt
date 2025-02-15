9dc8048de1f9594a555fa317f1d8af60658cf5ad
hyeseon <konkukbhs@gmail.com>
2025-02-15 22:14:13 +0900

    Revert "[feat/#34] ParsingOCR, GenerateReview 실패시 웹에게 빈 문자열 전달"
    This reverts commit aec5d0480a2c6b30f2baf7d17514f115c6c8fdd9.

--- feature/webview/src/main/java/com/nexters/misik/webview/WebViewViewModel.kt
+++ feature/webview/src/main/java/com/nexters/misik/webview/WebViewViewModel.kt
@@ -92,7 +92,6 @@ class WebViewViewModel @Inject constructor(
                 }
                 .onFailure { exception ->
                     _state.value = WebViewState.PageLoaded
-                    _responseJs.value = makeResponse("receiveScanResult", "")
                     Timber.d("parsingOcr_Failure", exception.message)
                 }
         }
@@ -140,13 +139,11 @@ class WebViewViewModel @Inject constructor(
                     jsonResponse.put("result", reviewText)
 
                     _responseJs.value =
-                        makeResponse("receiveGeneratedReview", "")
+                        makeResponse("receiveGeneratedReview", jsonResponse.toString())
 
                     Timber.d("getReview_Success", " ${data.isSuccess} $reviewText ${data.id}")
                 }
                 .onFailure { exception ->
-                    _responseJs.value =
-                        makeResponse("receiveGeneratedReview", "")
                     Timber.d("getReview_Failure", exception.message)
                 }
         }
