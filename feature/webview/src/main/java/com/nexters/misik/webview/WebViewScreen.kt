716b7d85aac6d6e017acb2f52135c90cc7f37264
hyeseon <konkukbhs@gmail.com>
2025-04-24 17:42:03 +0900

    [refactor/#49] repeatOnLifecycle(STARTED) 로 백그라운드에서는 수집안되도록

--- feature/webview/src/main/java/com/nexters/misik/webview/WebViewScreen.kt
+++ feature/webview/src/main/java/com/nexters/misik/webview/WebViewScreen.kt
@@ -22,7 +22,10 @@ import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.WindowInsetsCompat
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import com.nexters.misik.core.ui.LocalPreviewService
 import com.nexters.misik.webview.base.MisikWebViewFactory
 import com.nexters.misik.webview.bridge.WebInterface
@@ -74,29 +77,32 @@ fun WebViewScreen(
             webInterface = webInterface,
             onWebError = { error -> viewModel.sendIntent(WebViewIntent.WebViewLoadFailed(error)) },
 
-        )
+            )
     }
 
+    val lifecycleOwner = LocalLifecycleOwner.current
+
     LaunchedEffect(Unit) {
-        viewModel.sideEffect.collect { effect ->
-            when (effect) {
-                is UiSideEffect.SendJs -> {
-                    val js = when (effect.function) {
-                        "receiveGeneratedReview" -> makeReviewResponse(
-                            effect.function,
-                            effect.value,
-                        )
+        lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.sideEffect.collect { effect ->
+                when (effect) {
+                    is UiSideEffect.SendJs -> {
+                        val js = when (effect.function) {
+                            "receiveGeneratedReview" -> makeReviewResponse(
+                                effect.function,
+                                effect.value,
+                            )
 
-                        "receiveKeyboardHeight" -> makeKeyboardHeightResponse(
-                            effect.function,
-                            effect.value,
-                        )
+                            "receiveKeyboardHeight" -> makeKeyboardHeightResponse(
+                                effect.function,
+                                effect.value,
+                            )
 
-                        else -> makeResponse(effect.function, effect.value)
+                            else -> makeResponse(effect.function, effect.value)
+                        }
+                        webView.evaluateJavascript(js, null)
+                        Timber.d("WebViewScreen_toJS_Success: $js")
                     }
-                    webView.evaluateJavascript(js, null)
-                    Timber.d("WebViewScreen_sendJS: $js")
-                    Timber.d("WebViewScreen_toJS_Success: $js")
                 }
             }
         }
