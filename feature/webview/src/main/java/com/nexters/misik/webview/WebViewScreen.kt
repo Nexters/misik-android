759008187206640457b8027020eeafcaac60d829
hyeseon <konkukbhs@gmail.com>
2025-04-24 17:42:46 +0900

    [chore]

--- feature/webview/src/main/java/com/nexters/misik/webview/WebViewScreen.kt
+++ feature/webview/src/main/java/com/nexters/misik/webview/WebViewScreen.kt
@@ -77,7 +77,7 @@ fun WebViewScreen(
             webInterface = webInterface,
             onWebError = { error -> viewModel.sendIntent(WebViewIntent.WebViewLoadFailed(error)) },
 
-            )
+        )
     }
 
     val lifecycleOwner = LocalLifecycleOwner.current
